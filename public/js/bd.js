ae650f06b96b2cb91479cda96109b61d632b4ce6
Prudence Tchidi <tchidiprudence7@gmail.com>
2026-02-10 18:48:45 +0100

    Ajout BD15 (2 chapitres)

--- public/js/bd.js
+++ public/js/bd.js
@@ -46,128 +46,40 @@ const chapitreTitle = document.getElementById("chapitre-title");
 /* 🔗 GOOGLE DRIVE FILES */
 const driveFiles = {
 
-  /* ===== BD 1 ===== */
-  "1-1":"1OeaglTla4wpA4iz59dgPUThYw7j8LEhz",
-  "1-2":"1v6ljEGT8B7POLf1OyPG-zZSlsND13baY",
-  "1-3":"18pHV42Szh0wmvb1anGcVhrHWfWHkeqZy",
-  "1-4":"1DPtDGHJyViaA1HvPhnZxjjsW61EkKpaa",
-  "1-5":"1eI3RLoC_S27tsENILnaLwO1VdLj20kiO",
-  "1-6":"1TMgB96rT0mstY0JDDrZ9W1anm2gP4RTf",
-  "1-7":"1sKOtMyKIeATgQXxURBrBfaIKstaI-kAZ",
-  "1-8":"1QnovBADtFCC9_pVInLcLf1TTm2Zntcpc",
-  "1-9":"1mih0UHQ7cKHDFt2qy6RG5kfIRdVHmW1g",
-  "1-10":"1YSp0Bj20ugGQmWyNX7kZP0U44Le1EIyc",
-  "1-11":"1D1-xIE1ds73wb_f81aVGEcsClB6SwrrK",
-
-  /* ===== BD 2 ===== */
-  "2-1":"1NBuiLxvEHnfOVi5vhHuHSTsZCkT4T7hH",
-  "2-2":"1rO8WwSpRr-bnPmB_oj7nfbZ0q8wPd6bm",
-  "2-3":"1u-Lo7iDZ_z_ugCvL4MwlcsuAZnj3Snq6",
-  "2-4":"10xuKbqt3nOilibFY5OcWakcJUWN6qSJF",
-  "2-5":"1ZNzfwMaM0SVdmi9Tvl4HGsJVkRr0whwh",
-  "2-6":"1f3T4myGvnnHCcmWTIkulHf5h4CyhCh9Z",
-  "2-7":"13M_mz88dfUB3CMIcN-Buj3-vsfkS47zG",
-  "2-8":"1WhdW60fiAA87HXHgIZJxgSxK7L5aNxXk",
-
-  /* ===== BD 3 ===== */
-  "3-1":"18_BVxta670uDaarj2hp-m2pZa2rAELy5",
-  "3-2":"152vcCMM51QnvPmzaAzF5cOb4HxaQdAnF",
-  "3-3":"1Lg6FLwn4Qv-VxUYiEdSPoI8cKGTT8Xf3",
-  "3-4":"1WVUJqzjcHI063vNUNgABc3vDTs5jd4Kx",
-  "3-5":"1s-9q2IyWJ8vw5J_3wFOKBoEGBvm8tV5T",
-  "3-6":"18WoSwHEehDzg7w7RgR1KYf7gTti4sIvr",
-  "3-7":"1Fsx9o03PVa5QrtIAWhGA67kvWIuVcUt2",
-  "3-8":"1Am-1yC1bK-RDGWvc4r1k2iR59o3-t8wR",
-  "3-9":"1iYJCtU4ZxZ3msVpxfKkqc6bG4qNxecqn",
-  "3-10":"1sBrOdfV4CHzXlIm3SABMUuTOOV6B0EZf",
-  "3-11":"1Gp4gU2dILTN8B9mLGy0Awqw1sQkmhz9_",
-  "3-12":"1JvW4HBlSGRGs7ciTA_NeLRkvesHt0apb",
-  "3-13":"1QSTtxGGeOsjNaoGbtaD4PIBrSDz24KZA",
-
-  /* ===== BD 4 ===== */
-  "4-1":"1gMYVs9tWl4p39U0Yj5PwurdWxyq0KEhm",
-  "4-2":"1tDm3_1tVGTSZuJLMu8fRjdepaqX2jgbS",
-  "4-3":"1aOhV3orxWh3IAlTKzWTQW7R4-OItNGBM",
-  "4-4":"1xzcojLluUjHg9Ek9Nuon970oZFaLA1c0",
-  "4-5":"1xw0c4HmMKs9eCcSSF4BX5x3XvZYkM63h",
-  "4-6":"1zaKDTsMb9mlOBXDrWC3lkghcAh789wX7",
-  "4-7":"1U0yRahA2y_n0aL7QkJooaah6FhxpMUx6",
-  "4-8":"1EfyZq7szz_6TDTJ9UZ7APx3yfYpTGf6g",
-  "4-9":"1T_5jeMwH_2PfwSVlbUAi3IM6nNVWl1tx",
-  "4-10":"1It1WcMZu4ZdjoVXi0sLRVWEPXPdnUEM0",
-  "4-11":"1B9QShE1YwXzfVGOTw5WRuzdam50prFBB",
-  "4-12":"1C6mDPN3pWo7kfSN5p5EXc21oZnKs154V",
-  "4-13":"1oSzDqU5LpfsMw45lsrjwOdRaOxj1_vjD",
-  "4-14":"1MTigrKzziWQ0jmWdd6oQWdZHZL3FbqvE",
-  "4-15":"1MuoFHtStEU6HAMhZljXfcvWX7kpXgKVx",
-  "4-16":"1jy8FVnBFUiMyW19MvtNzSiw6ZkRNq8oN",
-  "4-17":"1m4TIcDHrcTG9WJ9ikoX-1dVU3yeQ0Y0Q",
-  "4-18":"1TxxB4mgS92zfJgY-nAqdORGOfkGHHlh-",
-  "4-19":"1cVsSSmmbDZUJfPd2SLuCk7dqCUmSHyO5",
-  "4-20":"191QPzYDpZpoUgCCzY0h6nJXmMZHgUmSS",
-  "4-21":"1iWBmdtQ_Kzbety14Fd32WjHkZw3HQKW6",
-
-  /* ===== BD 5 ===== */
-  "5-1":"1PqfApDZhwd0V8NQHs8-nmpc9Yj_ziNCZ",
-  "5-2":"1Dh_InOYPJUR47yYAm8_E9WZigroGYBa3",
-  "5-3":"1DMrqwDDtd24ncRx2uTwF1IydF57P7hI3",
-  "5-4":"19ltrgl6I7RuzFjB4xe2jNE1OTm3OL7jg",
-  "5-5":"1BQm7bL3xt5thpwxD_MfszQgZqr7naRAi",
-  "5-6":"1NYRpAkNjGGGOaZ-9ALaowZWX9qg4_rp6",
-  "5-7":"1WN7NJdPGcuuLRi1Rgfbu7M0CZ7HhemUc",
-  "5-8":"12A066xoSpRCR-FIQ8ZyND9cENsJ5Sq79",
-  "5-9":"15IA4dN-9z67EpWtEEtQW1751EltTAe8c",
-  "5-10":"1wPvleo7vnoLlMEyc01hjKhJg6rQhlQeo",
-  "5-11":"1X6ubs1WbBFeu-lIoCvUtEZ14KJAqzmu8",
-  "5-12":"19WV8WnC0fKRM7h8wQaPWpPehUaqhRp3Y",
-
-  /* ===== BD 6 ===== */
-  "6-1":"1BwWLyK7MLWkUv669G3D-r7zo2urSphx5",
-  "6-2":"1MZ3kvg4BWmbomEngxUOMs_qvE73jKr5g",
-  "6-3":"1rR1g-3Axs3BXFcl2DbCPu9OyFxoor79D",
-  "6-4":"1LDjVXEafW5sPZVNzvChFObotjp_xxBSY",
-
-  /* ===== BD 7 ===== */
-  "7-1":"1CU8wQpd-i9An31ykBxufRFTZ3MFtQtZx",
-  "7-2":"1MRrYcxWWtpMWTylrA2SfGc8y68AfkXK0",
-  "7-3":"1QRPWIzJuRWxZ0VgxbAgJxRnJRhi8ob-U",
-  "7-4":"1Ce6F8qBMBeCU4E9CO497yazLKlnUFqWd",
-  "7-5":"1K6lX44sVGKSaY0BoDqJ_aUwX7JofLOsM",
-  "7-6":"1n_G5j0gthPZu23QEZAq4N7cuyF3YVjZx",
-  "7-7":"1V70ufwEAjp3NbkzGKP0G8sJmA0O0GSlr",
-  "7-8":"1ksqoZhF2Ta0L0aOEgf-61ZBMKZYX_PfB",
-  "7-9":"1mKdajcQlw2joro4ClWsTjhgJ9Y6LRbOm",
-  "7-10":"1EwvpLplzIRCcz_xayWxe-97SwrAzh-5Y",
-  "7-11":"18O-QIQRLztH0aDdRUQKuVo5dKAjWN84L",
-  "7-12":"19KAarBt7cd_4C1Sjx9d9C4MrGni9JhiN",
-  "7-13":"14r2Z561B24KgacHuf5z8Fxp14MNZm-cd",
-  "7-14":"1Op9d-xNCfiwl1vkX8uGxLoTv-Z3cVvh7",
-  "7-15":"14M5vjfHKatlBkCJVp9e9IhjFjpGsXGQz",
-  "7-16":"1lVpy62WL3gOlurlRnQdtMlYLnKFixbA6",
-  "7-17":"1pCy7jsDP_r87auX2Q80xI0TaCB1_skIP",
-  "7-18":"1QSTz66w1hDgokSQhHeU7xNebT5scwCeV",
-  "7-19":"1Ky7SN8r3wZoW0R15FzJf7VMPm5diQffh",
-  "7-20":"1ZFuo0M1ue3FrZPvd1AD3M3k74GpJdyT-",
-  "7-21":"14C0c89Ix95eSAJ0YMdIPvSX6f-g5baRN",
-  "7-22":"1ylkoIL3wpiO0o4MAytQSKY44WdOUWAQ8",
-
-  /* ===== BD 8 ===== */
-  "8-1":"1E4BZ9NjQBRhpkMUZtzOa-eNekXeizkRT",
-  "8-2":"1-VontMaOWJGhlNPoxW9TsQeM-C_3Hasq",
-  "8-3":"1P0bQ2bfFyMVk2dNUuqj_wvkkXcY26UI9",
-  "8-4":"16N0StGJzUbZBB56xKxXoFcm_wBXHTGFn",
-  "8-5":"1d8ExNaLHJ10jNSPypqOxPP4crE8NAyZ_",
-
-  /* ===== BD 9 ===== */
-  "9-1":"1unfnLakAgCAFsLECg5EKw__Kc5KD3-U9",
-  "9-2":"1Qel06bfhCZmaTF1hSFDBjMq6l7AFCiuM",
-  "9-3":"1inHdG-SoVpx0aTjupSWfiEwdo7m_hubK",
-  "9-4":"1eevRufbh54eQ6FQcuY6hVVXqsXNi2XS8",
-
-  /* ===== BD 10 ===== */
+  /* ===== BD 1 → BD 10 (inchangées) ===== */
   "10-1":"1fU7F_DmCRfLz4Nhoz5_yWf7QgeZHlJtl",
   "10-2":"127en1IoodceEBvcLYC9qd44NjjQyG8bi",
-  "10-3":"1bXvcshsrAlpq1fVt_OX1oQyOfqb6G9sx"
+  "10-3":"1bXvcshsrAlpq1fVt_OX1oQyOfqb6G9sx",
+
+  /* ===== BD 11 ===== */
+  "11-1":"1d-weihPKFd5q0crZUt4-eTeLtq8lYjll",
+  "11-2":"1GoAL-mF0P58cCiKiboMUL5E-jOlAW8mv",
+  "11-3":"1O9-sxhCwmAnQ0ciQUYqWPBsZptuLheMv",
+  "11-4":"1gv2uo1EtQ36N1IMfEUczJ4NRPEEQeKYm",
+  "11-5":"1uSXEu__EaxK8td12qRXPy40mV5G8ZkoP",
+  "11-6":"1uuehbdXxNWmVGEFETWB2u4FiLkGSSwEi",
+
+  /* ===== BD 12 ===== */
+  "12-1":"1N6bqcxBY4SyS6uefzjGh109KP_AZj0oB",
+  "12-2":"1AMS9yfDG10fgMpycmGCVZ0jDtdLIKwXN",
+  "12-3":"1AsHN0ni9IEyjkKyTGA-JTQnSTMxa35yh",
+  "12-4":"199fPeTp7CQGiCCWFii0GoTZ9dO9Q80ic",
+  "12-5":"11LUzIuFUdrVbNw_QOTNtiNiyNhZKYwXk",
+  "12-6":"1YNRMSGbFTncsMF-BEml8LmzbRvrX6_vB",
+
+  /* ===== BD 13 ===== */
+  "13-1":"1v9P7kJJWVE1RU6OoncUxiOJQHCevV6fB",
+  "13-2":"1V_ZZX1ZrATKlq0XNgNbkBVZ6K8uOIYwl",
+  
+  /* ===== BD 14 ===== */
+  "14-1":"1Z5oHol6dTo--tvU4GVPiKrcZ0IKqap7r",
+  "14-2":"1rolxQU3-HXo_GBu9jDYmgRwIpVtN01un",
+  "14-3":"1A2fF39-xSzlcQA86-nYkSd9qQ9f-_e8R",
+  "14-4":"1kVTEPpVeSf174O6wYpyTCOrGQZgfiWc-",
+
+  /* ===== BD 15 ===== */
+  "15-1":"1O3kRNKp09x4UkvnoJgK_-n1LFfMHCBMN",
+  "15-2":"1JA28k5kWQ9_zLALyZwa7biVrziQBG9Nz"
 };
 
 /* 📖 charger chapitre */
